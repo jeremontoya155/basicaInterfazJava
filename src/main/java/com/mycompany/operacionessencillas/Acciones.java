924e98081fc139924e652bed14f07385d54265fc
Jere <jeremontoya155@gmail.com>
2022-01-15 02:01:54 -0300

    Primer ejemplo de interfaz grafica

---
+++ src/main/java/com/mycompany/operacionessencillas/Acciones.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.operacionessencillas;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Acciones {
+    
+    public double result;
+    
+    public void calcSuma(double n1,double n2){
+    
+        result=n1+n2;
+    }
+   
+     public void calcResta(double n1,double n2){
+    
+        result=n1-n2;
+    }
+   
+      public void calcMult(double n1,double n2){
+    
+        result=n1*n2;
+    }
+   
+       public void calcDiv(double n1,double n2){
+    
+        result=n1/n2;
+    }
+   
+    
+}
